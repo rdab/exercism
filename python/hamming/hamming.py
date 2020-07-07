f975619f1342323382bd95a2481ee09a825e7a16
rdab <rdamian.alfonso@riseup.net>
2020-07-07 19:40:48 -0400

    Replace map with list comprehension

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,4 +1,4 @@
 def distance(strand_a, strand_b):
     if len(strand_a) != len(strand_b):
         raise ValueError('Strand length differ')
-    return sum(map(lambda x,y: x!=y, iter(strand_a), iter(strand_b)))
+    return sum([x!=y for x,y in zip(strand_a, strand_b)])
