f61a5762dd184cd16b31933f8fb1d816185ab896
rdab <rdamian.alfonso@riseup.net>
2020-07-16 13:35:07 -0400

    refactor variable names

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,5 +1,6 @@
 from string import ascii_lowercase
 
+
 def is_isogram(string):
-    s = [l for l in string.lower() if l in ascii_lowercase]
-    return len(s) == len(set(s))
+    string = [letter for letter in string.lower() if letter in ascii_lowercase]
+    return len(string) == len(set(string))
