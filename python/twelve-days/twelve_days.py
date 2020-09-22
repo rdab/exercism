dc32049acbbac4b23a9f322a2d7ddcde770b6efe
rdab <rdamian.alfonso@riseup.net>
2020-09-21 23:58:22 -0400

    Add function

--- python/twelve-days/twelve_days.py
+++ python/twelve-days/twelve_days.py
@@ -1,26 +1,44 @@
 VERSE_START = "On the {ordinal} day of Christmas my true love gave to me:"
+VERSE_LAST = "a Partridge in a Pear Tree."
 
-d = {
-    1: "a Partridge in a Pear Tree.",
-    2: "two Turtle Doves, ",
-    3: "three French Hens, ",
+VERSES = {
+    2: "two Turtle Doves,",
+    3: "three French Hens,",
+    4: "four Calling Birds,",
+    5: "five Gold Rings,",
+    6: "six Geese-a-Laying,",
+    7: "seven Swans-a-Swimming,",
+    8: "eight Maids-a-Milking,",
+    9: "nine Ladies Dancing,",
+    10: "ten Lords-a-Leaping,",
+    11: "eleven Pipers Piping,",
+    12: "twelve Drummers Drumming,",
 }
 
-ORDINALS = {1: "first", 2: "second", 3: "third", 4: "fourth", 5: "fifth"}
+ORDINALS = {
+    1: "first",
+    2: "second",
+    3: "third",
+    4: "fourth",
+    5: "fifth",
+    6: "sixth",
+    7: "seventh",
+    8: "eighth",
+    9: "ninth",
+    10: "tenth",
+    11: "eleventh",
+    12: "twelfth",
+}
 
-verses = [
-    ("first", "a Partridge in a Pear Tree."),
-    ("second", "two Turtle Doves,"),
-    ("third", "three French Hens,"),
-    ("fourth", "four Calling Birds,"),
-]
+
+def create_verse(number):
+    verse = [VERSE_START.format(ordinal=ORDINALS[number])]
+    verse += [VERSES[n] for n in range(number, 1, -1)]
+    if number > 1:
+        verse += ["and"]
+    verse += [VERSE_LAST]
+    return " ".join(verse)
 
 
 def recite(start_verse, end_verse):
-    result = [VERSE_START.format(ordinal=verses[start_verse-1][0])]
-    v = [verses[n-1][1] for n in range(end_verse, 1, -1)]
-    result += v
-    if end_verse > 1:
-        result += ['and']
-    result += [verses[0][1]]
-    return [' '.join(result)]
+    return [create_verse(n) for n in range(start_verse, end_verse + 1)]
